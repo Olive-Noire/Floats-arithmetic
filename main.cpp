cdf9f6b90175ee92c5aabbc1c537917260ae7a37
Olive <69757487+Olive-Noire@users.noreply.github.com>
2021-11-27 21:00:12 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
-#include <iostream>
+template <typename Number = double> constexpr Number Pi{3.1415926535};
 
 double Cos(double x, unsigned long long p = 8) {
 
-    constexpr double pi{3.1415926535};
-    x -= static_cast<unsigned long long>(x/pi)*pi;
+    x -= static_cast<unsigned long long>(x/Pi<double>)*Pi<double>;
     
     double sum{0};
 
@@ -46,13 +45,13 @@ double Cos(double x, unsigned long long p = 8) {
 
     }
 
-    x -= static_cast<unsigned long long>(x/(pi*2))*(pi*2);
+    x -= static_cast<unsigned long long>(x/(Pi<double>*2))*(Pi<double>*2);
 
     if (x == 0) {
         
         return -1;
         
-    } else if (pi > x) {
+    } else if (Pi<double> > x) {
 
         return 1-sum;
 
@@ -64,11 +63,11 @@ double Cos(double x, unsigned long long p = 8) {
 
 }
 
-double Sin(double x, unsigned long long p = 8) { return Cos(x-3.1415926535/2, p); }
+double Sin(double x, unsigned long long p = 8) { return Cos(x-Pi<double>/2, p); }
 double Tan(double x, unsigned long long p = 8) { return Sin(x, p)/Cos(x, p); }
 double Cot(double x, unsigned long long p = 8) { return 1/Tan(x, p); }
 double Sec(double x, unsigned long long p = 8) { return 1/Cos(x, p); }
-double Csc(double x, unsigned long long p = 8) { return Sec(x-3.1415926535/2, p); }
+double Csc(double x, unsigned long long p = 8) { return Sec(x-Pi<double>/2, p); }
 
 double Power(double x, long long p) {
 
@@ -140,12 +139,3 @@ template <unsigned long long Base> double Root(double n, double precision = 0.1)
 
 double Sqrt(double x, double precison = 0.1) { return Root<2>(x, precison); }
 double CubeRoot(double x, double precison = 0.1) { return Root<3>(x, precison); }
-
-int main() {
-
-    for (std::size_t i{0}; i < 100; i++) std::cout << "sqrt(" << i << ") = " << Sqrt(i) << std::endl;
-    std::cin.get();
-
-    return 0;
-
-}
